7a3a82a19d36f3410cd225d5f917f571517e804b
BADXsouL <szhecka128@mail.ru>
2021-07-06 00:23:13 +0500

    RuslanBalbes

---
+++ PCAccessories.Web/wwwroot/script/script.js
@@ -0,0 +1,23 @@
+const btn = document.getElementById('SignUp');
+const login = document.getElementById('username');
+const mail = document.getElementById('mail')
+const password = document.getElementById('password');
+const confirmpassword = document.getElementById('confirmpassword');
+
+function signUp() {
+    fetch('http://localhost:3161/api/register', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify({
+        username:login.value,
+        password: password.value,
+        confirmpassword: confirmpassword.value,
+        email: mail.value,
+    })
+    })
+    .then((data) => {
+        console.log(data);
+    });
+}
