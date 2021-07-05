3442b4158e44990bf4896ef1c9fa1f0c682fea63
BADXsouL <szhecka128@mail.ru>
2021-07-05 21:26:05 +0500

    228

--- PCAccessories.Web/wwwroot/test/script.js
+++ PCAccessories.Web/wwwroot/test/script.js
@@ -3,9 +3,10 @@
 const btn = document.getElementById('btn');
 const xml = new XMLHttpRequest;
 
+
 xml.open('GET', 'http://localhost:3161/api/users');
 xml.addEventListener('load', () => {
-    console.log("LOAD SUCCESS")
+    console.log('LOAD SUCCESS');
 });
 
 xml.send();
