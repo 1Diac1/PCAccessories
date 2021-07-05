d5d68c89312a7d518048a314f3d8a9fab729caad
BADXsouL <szhecka128@mail.ru>
2021-07-05 23:41:43 +0500

    228

--- PCAccessories.Web/wwwroot/script/registr.js
+++ PCAccessories.Web/wwwroot/script/registr.js
@@ -1,27 +1,21 @@
-const btn = document.querySelector('.btn');
+const btn = document.getElementById('SignUp');
 const login = document.getElementById('username');
+const mail = document.getElementById('mail')
 const password = document.getElementById('password');
 
-
-function getRequests(callBack) {
-    const xmlRequest = new XMLHttpRequest;
-    xmlRequest.open("GET", "http://localhost:3161/api/users");
-    
-    xmlRequest.addEventListener('load', () => {
-    const requests = JSON.parse(xmlRequest.responseText);
-    callBack(requests)
-    });
-
-    xmlRequest.send();
-}
-function checkInput() {
-    btn.addEventListener('click', () => {
-        getRequests((requests) => {
-    
-            requests.forEach(posts => {
-                console.log(requests)
-                
-            });
-        });
+function signUp() {
+    fetch('http://localhost:3161/api/register', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+    },
+    body: JSON.stringify({
+        name:login.textContent,
+        password: password.textContent,
+        email: mail.textContent
+    })
+    })
+    .then((data) => {
+        console.log(data);
     });
 }
