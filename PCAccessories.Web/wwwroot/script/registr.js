f78bd53c44793004a155130374b89b2850f7bd7a
BADXsouL <86883357+BADXsouL@users.noreply.github.com>
2021-07-07 01:15:23 +0500

    Update registr.js

--- PCAccessories.Web/wwwroot/script/registr.js
+++ PCAccessories.Web/wwwroot/script/registr.js
@@ -1,35 +1,36 @@
-const login = document.getElementById('username');
-const mail = document.getElementById('mail')
-const password = document.getElementById('password');
-const confirmpassword = document.getElementById('confirmpassword');
-let addAccessToken, addRefreshToken, errorUp;
+const checkLogin = document.getElementById('signLogin');
+const checkPass = document.getElementById('signPass');
+let accessToken, refreshToken;
 
-function signUp(){ 
-    fetch('http://localhost:3161/api/register',{
+function signIn(){
+    fetch('http://localhost:3161/api/login',{
     method: 'POST',
     headers: {
-        'Content-Type': 'application/json;charset=utf8'
+        'Content-Type': 'application/json;charset=utf-8'
     },
     body: JSON.stringify({
-        username: login.value,
-        password: password.value,
-        email: mail.value,
-        confirmpassword: confirmpassword.value
+        username: checkLogin.value,
+        password: checkPass.value,
     })
     })
     .then(response => response.json())
     .then(json => callBack(json))
-}
+} 
 function callBack(x) {
-    addAccessToken = x.accessToken;
-    addRefreshToken = x.refreshToken;
+    accessToken = x.accessToken;
+    refreshToken = x.refreshToken;
     try {
-        if(addAccessToken != null && addRefreshToken != null) {
-            localStorage.setItem('accessToken', addAccessToken);
-            localStorage.setItem('refreshToken', addRefreshToken);
+        if(accessToken != null && refreshToken != null) {
+            localStorage.setItem('accessToken', accessToken);
+            localStorage.setItem('refreshToken', refreshToken);
             window.location.replace('home.html');
         }
-    } catch (error) {
-        console.log(error)
+        alert(error)
+    } catch(err) {
+        console.log(err)
     }
+}
+let token = localStorage.getItem('accessToken');
+if(token != null) {
+    window.location.replace('home.html');
 }
