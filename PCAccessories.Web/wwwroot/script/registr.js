a263276492100c95464b28d59dea9cfb12a852a1
BADXsouL <szhecka128@mail.ru>
2021-07-06 21:29:35 +0500

    228

--- PCAccessories.Web/wwwroot/script/registr.js
+++ PCAccessories.Web/wwwroot/script/registr.js
@@ -3,6 +3,7 @@ const mail = document.getElementById('mail')
 const password = document.getElementById('password');
 const confirmpassword = document.getElementById('confirmpassword');
 let addAccessToken, addRefreshToken;
+let errors = [];
 
 function signUp() {
     fetch('http://localhost:3161/api/register', {
@@ -23,14 +24,15 @@ function signUp() {
 function callBack(x) {
     addAccessToken = x.accessToken
     addRefreshToken = x.refreshToken
+    errors = x.errors;
     try {
         if(addAccessToken != null && addRefreshToken != null) {
             localStorage.setItem('accessToken', addAccessToken);
             localStorage.setItem('refreshToken', addRefreshToken);
             window.location.replace('home.html');
         }
-    } catch(err) {
-        alert(err)
+    } catch(errors) {
+        alert(errors)
     }
 }
 let tokenRegister = localStorage.getItem('accessToken');
