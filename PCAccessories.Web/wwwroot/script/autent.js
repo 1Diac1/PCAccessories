4534cb676e52c74c6930c503ecdfbf757ac7d49e
BADXsouL <szhecka128@mail.ru>
2021-07-06 23:50:55 +0500

    228

--- PCAccessories.Web/wwwroot/script/autent.js
+++ PCAccessories.Web/wwwroot/script/autent.js
@@ -1,32 +1,4 @@
 const checkLogin = document.getElementById('signLogin');
 const checkPass = document.getElementById('signPass');
-let accessToken, refreshToken, error;
+let accessToken, refreshToken, errorIn;
 
-function signIn() {
-	fetch('http://localhost:3161/api/login', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json;charset=utf-8'
-    },
-    body: JSON.stringify({
-        username: checkLogin.value,
-        password: checkPass.value,
-    })
-    })
-    .then(response => response.json())
-    .then(json => callBack(json))
-} 
-function callBack(x) {
-    accessToken = x.accessToken;
-    refreshToken = x.refreshToken;
-    error = x.errors;
-    try {
-        if(accessToken != null && refreshToken != null) {
-            localStorage.setItem('accessToken', accessToken);
-            localStorage.setItem('refreshToken', refreshToken);
-            window.location.replace('home.html');
-        }
-    } catch(err) {
-        console.log(err)
-    }
-}
--- PCAccessories.Web/wwwroot/script/registr.js
+++ PCAccessories.Web/wwwroot/script/registr.js
@@ -2,42 +2,15 @@ const login = document.getElementById('username');
 const mail = document.getElementById('mail')
 const password = document.getElementById('password');
 const confirmpassword = document.getElementById('confirmpassword');
-let addAccessToken, addRefreshToken, errorsUp;
+let addAccessToken, addRefreshToken, errorUp;
 
-function signUp() {
-    fetch('http://localhost:3161/api/register', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json;charset=utf-8'
-    },
-    body: JSON.stringify({
-        username:login.value,
-        password: password.value,
-        confirmpassword: confirmpassword.value,
-        email: mail.value,
+axios.post('http://localhost:3161/api/register', {
+    username:login.value,
+    password: password.value,
+    confirmpassword: confirmpassword.value,
+    email: mail.value,
     })
-    })
-    .then(response => response.json())
-    .then(json => callBack(json))
-} 
-function callBack(x) {
-
-    addAccessToken = x.accessToken;
-    addRefreshToken = x.refreshToken;
-    
-
-    try {
-        if(addAccessToken != null && addRefreshToken != null) {
-            localStorage.setItem('accessToken', addAccessToken);
-            localStorage.setItem('refreshToken', addRefreshToken);
-            window.location.replace('home.html');
-        }
-    } 
-    catch(err) {
-        alert(err)
-    }
-}
-let tokenRegister = localStorage.getItem('accessToken');
-if(tokenRegister != null) {
-    window.location.replace('../test/index.html');
-}
+    .then(res => {
+        alert('Заявка отправлена');})
+    .catch(function (error) {
+        alert(error)})
