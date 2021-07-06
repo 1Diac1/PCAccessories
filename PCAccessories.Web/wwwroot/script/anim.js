375317f93fe0f65012f1ff5f41fdc7c03b6508cd
BADXsouL <szhecka128@mail.ru>
2021-07-06 16:13:40 +0500

    2828

--- PCAccessories.Web/wwwroot/script/anim.js
+++ PCAccessories.Web/wwwroot/script/anim.js
@@ -1,14 +1,9 @@
-const btn_signUp = document.getElementById('signUp');
-const btn_signIn = document.getElementById('signIn');
-const container = document.getElementById('container');
-
-btn_signUp.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
-});
-
-btn_signIn.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
-});
+$('#signUp').click(() => {
+	$('#container').addClass('right-panel-active')
+})
+$('#signIn').click(() => {
+	$('#container').removeClass('right-panel-active')
+})
 
 
  
