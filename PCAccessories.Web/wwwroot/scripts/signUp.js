773ff98275b5aa3afda9900a92d0879c03541000
Eugene EBOY <szhecka128@mail.ru>
2021-07-26 01:05:36 +0300

    js_again_not_important

--- PCAccessories.Web/wwwroot/scripts/signUp.js
+++ PCAccessories.Web/wwwroot/scripts/signUp.js
@@ -1,22 +1,22 @@
-$("#submit").click(function (e) {
-  e.preventDefault();
-  let data = {
-    Login: $("#username").val(),
-    Email: $("#mail").val(),
-    Password: $("#password").val(),
-    ConfirmPassword: $("#confirmpassword").val(),
-  };
-
-  $.ajax({
-    type: "POST",
-    url: "api/v1/auth/register",
-    contentType: "application/json; charset=utf-8",
-    data: JSON.stringify(data),
-  })
-    .success(function (data) {
-      alert("Регистрация пройдена");
+$(function () {
+        $('#submit').click(function (e) {
+            e.preventDefault();
+            let data = {
+                Login: $('#username').val(),
+                Email: $('#mail').val(),
+                Password: $('#password').val(),
+                ConfirmPassword: $('#confirmpassword').val()
+            };
+ 
+            $.ajax({
+                type: 'POST',
+                url: 'api/v1/auth/register',
+                contentType: 'application/json; charset=utf-8',
+                data: JSON.stringify(data)
+            }).success(function (data) {
+                alert("Регистрация пройдена");
+            }).fail(function (data) {
+                alert("В процесе регистрации возникла ошибка");
+            });
+        });
     })
-    .fail(function (data) {
-      alert("В процесе регистрации возникла ошибка");
-    });
-});
