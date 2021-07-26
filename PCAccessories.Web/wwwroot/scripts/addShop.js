36a162db76b75af51a5da2dfab28b36ed09141d9
Eugene EBOY <szhecka128@mail.ru>
2021-07-27 00:29:50 +0300

    addProducts js

---
+++ PCAccessories.Web/wwwroot/scripts/addShop.js
@@ -0,0 +1,25 @@
+window.onload(() => {
+  e.preventDefault();
+  let data = {
+    title: titleAdd,
+    description:descriptionAdd,
+    price: priceAdd,
+    isAvailable: isAvailableAdd,
+  };
+
+  $.ajax({
+    type: "GET",
+    url: "http://localhost:3161/api/v1/products",
+    contentType: "application/json; charset=utf-8",
+    data: JSON.stringify(data),
+  })
+    .success(function (data) {
+      alert("Все добавилось!");
+      alert(data);
+    })
+    .fail(function (data) {
+      alert(data);
+    });
+});
+
+let titleAdd, descriptionAdd, priceAdd, isAvailableAdd;
