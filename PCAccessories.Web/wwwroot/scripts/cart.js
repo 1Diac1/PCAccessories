23241b02bde71f9ecaed87c9a64dee58641c6503
Eugene EBOY <szhecka128@mail.ru>
2021-07-27 13:32:44 +0300

    js fix cart

--- PCAccessories.Web/wwwroot/scripts/cart.js
+++ PCAccessories.Web/wwwroot/scripts/cart.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
   $('.bminus').click(function(e){
   e.preventDefault();
-  var $this = $(this);
-  var input = $('#counter');
-  var value = parseInt(input.val());
+  let $this = $(this);
+  let input = $('#counter');
+  let value = parseInt(input.val());
   if(value > 1){value = value - 1;}
   else{value = 0;}
   input.val(value);
@@ -11,9 +11,9 @@ $(document).ready(function(){
   
   $('.bplus').click(function(e){
   e.preventDefault();
-  var $this = $(this);
-  var input = $('#counter');
-  var value = parseInt(input.val());
+  let $this = $(this);
+  let input = $('#counter');
+  let value = parseInt(input.val());
   if(value < 100){value = value + 1;}
   else{value = 100;}
   input.val(value);
