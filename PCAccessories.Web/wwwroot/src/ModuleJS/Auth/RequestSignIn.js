d6f94f082d88df8ec7053501fdff3823723748ca
Eugene EBOY <szhecka128@mail.ru>
2021-07-28 22:53:37 +0300

    get req

--- PCAccessories.Web/wwwroot/src/ModuleJS/Auth/RequestSignIn.js
+++ PCAccessories.Web/wwwroot/src/ModuleJS/Auth/RequestSignIn.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function RequestSignIn() {
 
-  fetch('http://localhost:3161/api/v1/auth/login')
+  fetch('http://localhost:3161/api/v1/products')
     .then(response => response.json())
     .then(json => console.log(json))
 
