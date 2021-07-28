a74f95547036f5c07f791b46463ecc3bf984e36d
Eugene EBOY <szhecka128@mail.ru>
2021-07-28 20:28:33 +0300

    axios req post

--- PCAccessories.Web/wwwroot/src/ModuleJS/Auth/RequestSignIn.js
+++ PCAccessories.Web/wwwroot/src/ModuleJS/Auth/RequestSignIn.js
@@ -1,11 +1,16 @@
 import React from "react";
+import axios from "axios";
 
 class RequestSignIn extends React.Component {
+
+    state = {
+
+    }
+
   componentDidMount() {
     const apiUrl = "http://localhost:3161/api/v1/auth/login";
-    fetch(apiUrl)
-      .then((response) => response.json())
-      .then((data) => console.log("This is your data", data));
+    axios.post(apiUrl)
+        .then(res => console.log(res));
   }
   render() {
     return <button>Войти</button>;
