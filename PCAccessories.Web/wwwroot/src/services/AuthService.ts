0045581066d600f7bd2fba8f2d52020555def4ae
Eugene BADXsouL <szhecka128@mail.ru>
2021-08-03 00:49:53 +0500

    change axios

--- PCAccessories.Web/wwwroot/src/services/AuthService.ts
+++ PCAccessories.Web/wwwroot/src/services/AuthService.ts
@@ -3,8 +3,8 @@ import $api from "../http";
 import { AuthResponse } from "../models/response/AuthResponse";
 
 export default class AuthService {
-     static async login(login: string, password: string): Promise<AxiosResponse<AuthResponse>> {
-         return $api.post<AuthResponse>('/auth/login', {login, password})
+     static async login(Username: string, password: string): Promise<AxiosResponse<AuthResponse>> {
+         return $api.post<AuthResponse>('/auth/login', {Username, password})
      }
      
      static async registration(login: string, password: string): Promise<AxiosResponse<AuthResponse>> {
